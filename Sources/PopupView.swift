6f2af790606e464ec7923c96c6005bf46319a37c
Shima <shima.jin@icloud.com>
2018-11-18 17:28:19 +0900

    Add base file

---
+++ Sources/PopupView.swift
@@ -0,0 +1,13 @@
+//
+//  PopupView.swift
+//  PopupView
+//
+//  Created by Jinsei Shima on 2018/11/18.
+//  Copyright © 2018 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class PopupView: UIView {
+    
+}
